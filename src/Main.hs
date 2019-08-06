03203df7a7f0561b1cea1d2fa62119bfeb29eb4f
doublescale <doublescale@tutanota.com>
2019-08-06 22:53:10 +0200

    output time difference

--- src/Main.hs
+++ src/Main.hs
@@ -89,9 +89,13 @@ showTimes :: IORef (Maybe Double) -> IORef (Maybe Double) -> IO ()
 showTimes ourRef theirRef = do
   ours <- readIORef ourRef
   theirs <- readIORef theirRef
-  printf "\rUs: %s  Them: %s" (render ours) (render theirs)
+  let diff = (-) <$> ours <*> theirs
+  printf "\rUs: %s  Them: %s  Diff:%s  "
+         (renderTime ours) (renderTime theirs) (renderDiff diff)
   hFlush stdout
   where
-    render Nothing = "--:--" :: String
-    render (Just t) = printf "%02d:%02.0f" (m :: Int) s
+    renderTime Nothing = "--:--" :: String
+    renderTime (Just t) = printf "%02d:%02.0f" (m :: Int) s
       where (m, s) = divMod' (max 0 t) 60
+    renderDiff Nothing = " ---" :: String
+    renderDiff (Just d) = printf "%+4.0f" d
